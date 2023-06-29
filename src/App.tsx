cd3587b48940e43fa4fc07b821afcffe7f4c5648
Charlscooper <charlescooper_@hotmail.com>
2023-06-29 22:44:43 +0100

    create-ui: Added - structure change

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
-import React from 'react';
 import './App.css';
-import { GetRepoData, get_repo_data } from './api/get-repo-data';
 import { Structure } from './components/Structure';
 import { AppProvider } from './data/context/AppContext';
 
 const App = () => {
-  //   const test: GetRepoData = {
-  //     owner: 'charlscooper',
-  //     repo_name: 'github-api-app',
-  //   };
-  //   const [repo_data, setRepoData] = React.useState(null);
-
-  //   React.useEffect(() => {
-  //     const update_data_stream = async () => {
-  //       const data = await get_repo_data({ ...test });
-  //       setRepoData(data);
-  //     };
-  //     console.log('update_data_stream');
-  //     console.log('repo_data : ', repo_data);
-  //     update_data_stream();
-  //   }, []);
-
-  //   return repo_data ? (
-  //     <div className="text-2xl">{JSON.stringify(repo_data)}</div>
-  //   ) : (
-  //     <div>Loading...</div>
-  //   );
   return (
     <div
       id="App"
